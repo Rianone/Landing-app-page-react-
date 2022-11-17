009cee8a1d2c7ae870f26b36f1e16cc77ac5dceb
Rianone <84690507+Rianone@users.noreply.github.com>
2022-11-17 21:55:46 +0100

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
 function Navbar() {
     return ( 
-        <nav class="navbar navbar-expand navbar-light bg-light d-flex align-items-center justify-content-center">
-        <ul class="nav navbar-nav">
-            <li class="nav-item active pt-2 pb-2 pe-4 ps-4 me-4">
-                <a class="nav-link" href="#home">Home</a>
+        <nav className="navbar navbar-expand navbar-light bg-light d-flex align-items-center justify-content-center">
+        <ul className="nav navbar-nav">
+            <li className="nav-item active pt-2 pb-2 pe-4 ps-4">
+                <a className="nav-link" href="#home">Home</a>
             </li>
-            <li class="nav-item me-4 pt-2 pb-2 pe-4 ps-4">
-                <a class="nav-link" href="#specials">Specials</a>
+            <li className="nav-item  pt-2 pb-2 pe-4 ps-4">
+                <a className="nav-link" href="#specials">Specials</a>
             </li>
-             <li class="nav-item me-4 pt-2 pb-2 pe-4 ps-4">
-                <a class="nav-link" href="#about">About</a>
+             <li className="nav-item  pt-2 pb-2 pe-4 ps-4">
+                <a className="nav-link" href="#about">About</a>
             </li>
-             <li class="nav-item me-4 pt-2 pb-2 pe-4 ps-4">
-                <a class="nav-link" href="#menu">Menu</a>
+             <li className="nav-item pt-2 pb-2 pe-4 ps-4">
+                <a className="nav-link" href="#menu">Menu</a>
             </li>
-             <li class="nav-item  pt-2 pb-2 pe-4 ps-4">
-                <a class="nav-link " href="#contact">Contact</a>
+             <li className="nav-item  pt-2 pb-2 pe-4 ps-4">
+                <a className="nav-link " href="#contact">Contact</a>
             </li>
         </ul>
       </nav>
